51add160c548102f950b53324ca59e541d595523
Josh Isaacs <Josh Isaacs@DESKTOP-N3NGI64>
2016-06-11 11:37:11 +0800

    Hooked in vault.

--- src/main/java/io/github/jisaacs1207/followers/Followers.java
+++ src/main/java/io/github/jisaacs1207/followers/Followers.java
@@ -2,16 +2,29 @@ package io.github.jisaacs1207.followers;
 
 import java.util.HashMap;
 import java.util.TreeMap;
+import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import net.milkbowl.vault.chat.Chat;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.economy.EconomyResponse;
+import net.milkbowl.vault.permission.Permission;
 
 import io.github.jisaacs1207.followers.Commands;
 import io.github.jisaacs1207.followers.PlayerConfig;
 
 public final class Followers extends JavaPlugin implements Listener{
+	//vault
+    private static final Logger log = Logger.getLogger("Minecraft");
+    public static Economy econ = null;
+    public static Permission perms = null;
+    public static Chat chat = null;
+	
+	//me
 	public static Followers plugin;
 	public static HashMap<String, PlayerConfig> playerStats = new HashMap<String, PlayerConfig>();
 	public static TreeMap<String, AvailableFollowers> availableFollowers = new TreeMap<String, AvailableFollowers>();
@@ -30,6 +43,14 @@ public final class Followers extends JavaPlugin implements Listener{
 		}
 		Methods.populateFollowers();
 		Schedules.startFollowerExchange();
+		//vault
+		if (!setupEconomy() ) {
+            log.severe(String.format("[%s] - Disabled due to no Vault dependency found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        setupPermissions();
+        setupChat();
 	}
 
 	@Override
@@ -44,4 +65,28 @@ public final class Followers extends JavaPlugin implements Listener{
             Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
         }
 	}
+	
+	private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
+
+    private boolean setupChat() {
+        RegisteredServiceProvider<Chat> rsp = getServer().getServicesManager().getRegistration(Chat.class);
+        chat = rsp.getProvider();
+        return chat != null;
+    }
+
+    private boolean setupPermissions() {
+        RegisteredServiceProvider<Permission> rsp = getServer().getServicesManager().getRegistration(Permission.class);
+        perms = rsp.getProvider();
+        return perms != null;
+    }
 }
